4d0aa722e7135d8863c8a4d0f11a1758015c78d5
John-GitHub <john.phillip.owens@gmail.com>
2017-03-12 17:40:59 -0500

    I did document. I'm not sure I get it.

--- R/aFunction.R
+++ R/aFunction.R
@@ -1,11 +1,11 @@
 
 
-#' Foo
+#' Foo a random function just as a test
 #'
 #' @param x An argument of any type
 #'
 #' @return x That argument right back at you
 #' @export
 
-foo<-function(x) {x}
+foo<-function(x) x
 
